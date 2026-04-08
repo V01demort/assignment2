daf4b2c221a4a1b2448bdc73cb2e6d91a7fc87ab
V01demort <251265@astanait.edu.kz>
2026-04-08 12:57:45 +0500

    1

--- src/Task6.java
+++ src/Task6.java
@@ -1,5 +1,4 @@
 public class Task6 {
-    // Класс BankAccount (тот же, что использовался раньше)
     static class BankAccount {
         String accountNumber;
         String username;
@@ -13,15 +12,12 @@ public class Task6 {
     }
 
     public static void main(String[] args) {
-        // Создаём массив из 3 аккаунтов (физическая структура)
         BankAccount[] accounts = new BankAccount[3];
 
-        // Заполняем 3 предопределённых аккаунта
         accounts[0] = new BankAccount("ACC001", "Ali", 150000);
         accounts[1] = new BankAccount("ACC002", "Sara", 220000);
         accounts[2] = new BankAccount("ACC003", "Omar", 100000);
 
-        // Выводим все аккаунты
         System.out.println("=== TASK 6: Physical Data Structures (Array) ===");
         System.out.println("Predefined accounts in Array:");
         for (int i = 0; i < accounts.length; i++) {
